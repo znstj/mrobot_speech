e774b4eaa42299c3b9493aab24bed39eadc93a7a
znstj <zhang_nan1@126.com>
2016-09-05 21:36:56 +0800

    master
    Run sh mrobot_speech-prereq.sh automatically add the folder permissions

--- mrobot_speech-prereq.sh
+++ mrobot_speech-prereq.sh
@@ -2,3 +2,5 @@
 
 # Install the prerequisites for the mrobot_speech 
 sudo apt-get install gstreamer0.10-pocketsphinx ros-indigo-pocketsphinx  ros-indigo-audio-common  libasound2 gstreamer0.10-gconf
+
+chmod +777 ./nodes/mrobot_voice_nav.py ./nodes/recognizer.py ./nodes/talkback.py 
